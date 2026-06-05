f09f462e3bcaadd5a63387b0d55d79f1f829634a
edmundkb <edmundkb@outlook.com>
2026-06-05 15:16:58 +0100

    initial commit

---
+++ src/main/java/com/crypto/alert/engine/CryptoAlertEngineApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.alert.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAlertEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoAlertEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto/alert/engine/CryptoAlertEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.alert.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAlertEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
